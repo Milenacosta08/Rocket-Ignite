6c30cdb680f4e99f7c2cd33ac373b163e1a7712c
Milenacosta08 <milena.costa8291@gmail.com>
2022-03-16 10:45:49 -0300

    :boom: Adição de autentificação e validação nos formulários

--- Capiter-IV/dashgo/src/components/Header/SearchBox.tsx
+++ Capiter-IV/dashgo/src/components/Header/SearchBox.tsx
@@ -1,7 +1,18 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
+// Controlled components
+// Uncontrolled components
+
 export function SearchBox() {
+    /*
+    const [search, setSearch] = useState('')
+        LÁ NO INPUT: value={search} onChange={event => setSearch(event.target.value)}
+    */
+
+    const searchInputRef = useRef<HTMLInputElement>(null)
+
     return (
         <Flex
             as="label"
@@ -23,6 +34,7 @@ export function SearchBox() {
                 mr="4"
                 placeholder='Buscar na plataforma'
                 _placeholder={{color: "gray.400"}}
+                ref = {searchInputRef}
             />
 
             <Icon as={RiSearchLine} fontSize="20" />
